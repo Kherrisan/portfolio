ca11773baec6c1d56b6a66cb86bf5c221919885e
kherrisan <zdkscope@qq.com>
2022-12-27 19:56:20 +0800

    Color 😃

--- config/friend.ts
+++ config/friend.ts
@@ -11,7 +11,7 @@ export const friends: FriendProps[] = [
     link: 'https://pearlchocolatezy.com/',
     avatar:
       'https://b2.kendrickzou.com/avatar.jpg',
-    bgColor: '#b59672',
+    bgColor: '#73B1E3',
   },
   {
     id: '@阿阿阿阿阿什么偶CiinDii橙',
@@ -24,35 +24,5 @@ export const friends: FriendProps[] = [
     link: 'https://weibo.com/u/5653101362',
     avatar: 'https://s3.bmp.ovh/imgs/2022/07/18/131f3d412fdec5de.jpg',
     bgColor: '#895969',
-  },
-  // {
-  //   id: '@Silvester',
-  //   link: 'https://silvester.wang',
-  //   avatar: 'https://avatars0.githubusercontent.com/u/34436920',
-  //   bgColor: '#595058',
-  // },
-  // {
-  //   id: '@Arvin',
-  //   link: 'https://isarvin.com',
-  //   avatar: 'https://avatars.githubusercontent.com/u/41031053',
-  //   bgColor: '#4c493e',
-  // },
-  // {
-  //   id: '@Louis',
-  //   link: 'https://islouis.com',
-  //   avatar: 'https://avatars.githubusercontent.com/u/47236598',
-  //   bgColor: '#889ABF',
-  // },
-  // {
-  //   id: '@idealclover',
-  //   link: 'https://idealclover.top',
-  //   avatar: 'https://avatars2.githubusercontent.com/u/24428416',
-  //   bgColor: '#487747',
-  // },
-  // {
-  //   id: '@Lancern',
-  //   link: 'https://lancern.xyz',
-  //   avatar: 'https://avatars.githubusercontent.com/u/29401356',
-  //   bgColor: '#f2ae72',
-  // },
+  }
 ]
